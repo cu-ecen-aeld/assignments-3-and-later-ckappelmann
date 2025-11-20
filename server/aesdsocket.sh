338fa43e087f582105d9746acded98a11b364bf8
Christopher Kappelmann <christopher.kappelmann@gmail.com>
2025-11-20 17:05:09 -0600

    Added aesdsocket.sh daemon script

---
+++ server/aesdsocket.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:          aesdserver
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: AESD socket server
+# Description:       Start/stop the AESD socket server daemon
+### END INIT INFO
+
+case "$1" in
+    start)
+        echo "Starting aesdsocket"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon -K -n aesdsocket
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart}"
+        exit 1
+        ;;
+esac
+
+exit 0
